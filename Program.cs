a5a51aedecc1fa7522f5cab129ecacab5c4a26f9
PC1-IFLAB <liyantravail@gmail.com>
2023-03-14 16:09:35 +0700

    Commit Pertama

--- Program.cs
+++ Program.cs
@@ -6,10 +6,21 @@ using System.Threading.Tasks;
 
 namespace MOD4_1302213037
 {
+    public class KodeBuah
+    {
+        public enum{
+            Apel,Aprikot,Alpukat,Pisang, Paprika,
+            Blackberry, Ceri,Kelapa,Jagung, Kurma, 
+            Durian,Anggur, Melon, Semangka 
+        };
+
+    } 
+
     internal class Program
     {
         static void Main(string[] args)
         {
+            
         }
     }
 }
