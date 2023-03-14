5911f3808413d4bcc0012bbe2e96e381ce67ab24
PC1-IFLAB <naziqnaz13@gmail.com>
2023-03-14 15:49:31 +0700

    commit pertama

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MOD4_1302213037
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
